37897c208a5a9a65d74e59e71299cca9a6d83e42
Shriyash Rulhe <shriyashrulhe145@gmail.com>
2024-02-27 11:36:10 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,13 @@
+const data = {
+    1: {
+      title: 'Option 1 And lord shriyash is a great man in the world and lord of lords is shriyash and his bestfriend is a very cheap',
+      image: 'https://img.freepik.com/free-photo/glasses-near-laptop-reflect-light-from-screen-dark-copy-space_169016-14893.jpg?t=st=1708858674~exp=1708862274~hmac=1f8435e8c7e8c47d1c356c0370520b9ba3038a45404287cced5264550649f569&w=826',
+      audio: '/audio/option1.mp3',
+      video: '/video/option1.mp4',
+      // Add other data as needed
+    },
+    // Add data for other options
+  };
+  
+  module.exports = data;
+  
